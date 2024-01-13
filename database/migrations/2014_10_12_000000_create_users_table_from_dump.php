061baf28b845b251c7d31525133d295c4dcac3a5
Bagrat <b.saatsazov@gmail.com>
2024-01-13 13:26:32 +0100

    Add comments and direction of work towards migrations.

---
+++ database/migrations/2014_10_12_000000_create_users_table_from_dump.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        // todo
+        throw new Exception("todo: correct path and check if it works.");
+
+        DB::unprepared(file_get_contents('full/path/to/dump.sql'));
+
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('name');
+        //     $table->string('email')->unique();
+        //     $table->timestamp('email_verified_at')->nullable();
+        //     $table->string('password');
+        //     $table->rememberToken();
+        //     $table->timestamps();
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // todo: drop
+        // Schema::dropIfExists('users');
+    }
+};
