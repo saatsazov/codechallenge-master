5d162d32af21d0f5642f49ce7f7756e59f516903
Bagrat <b.saatsazov@gmail.com>
2024-01-14 15:16:29 +0100

    remove not needed endpoints

--- routes/api.php
+++ routes/api.php
@@ -17,19 +17,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// todo: Move this code to controllers, services, repository. Basically organize it much nicer
+
+Route::get("/schedule", function (Request $request) {
+    // not optimal solution in terms of performance probably produces a lot of queries
+    $sessions = Session::with('participants.user')
+        ->where('active', 'Y')
+        ->get();
+
+
+    // At first I thought to do some processing in order to group response by user id in case of 
+    // multiple users may be at one conference with different role
+
+    // $its = $sessions->map(function (Session $item) {
+    //     $processedParticipants = $item->participants;
+    //     // $processedParticipants = $item->participants->groupBy('userid');
+    //     return [
+    //         ...$item->toArray(),
+    //         'participants' => $processedParticipants,
+    //     ];
+    // });
+    return [
+        'items' => $sessions,
+    ];
 });
 
-Route::get("/users", function (Request $request) {
-    return User::all();
-});
+
+Route::get("/schedule_v2", function (Request $request) {
+    // TODO: I can more effectively query DB with two requests and join them on php side. But out of scope for now
+    $sessions = Session::select(
+        "name",
+        "session_start",
+        "session_end",
+    )
+        ->where('active', 'Y')
+        ->get();
 
 
-Route::get("/roles", function (Request $request) {
-    return Role::all();
-});
-
-Route::get("/sessions", function (Request $request) {
-    return Session::where('active', 'N')->get();
+    throw new Exception("not implemented");
+    $participants = User::select(
+        "s.id",
+        "u.name",
+        "r.usertype",
+    )->join("role r");
+    return [
+        'items' => $sessions,
+    ];
 });
